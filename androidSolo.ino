ed4b16e4bd93cf90c6496e960ba5a3101235e712
Iulian-Alexandru Dandu <dandu.ialexandru@gmail.com>
2024-04-22 00:17:16 +0300

    commit

--- androidSolo.ino
+++ androidSolo.ino
@@ -1,43 +1,39 @@
-// constants won't change. They're used here to set pin numbers:
-
-const int buttonPin =  4;     // the number of the pushbutton pin
+const int buttonPin =  4;     
 const int danceButton = 3;
-const int ledPin        = 13;      // the number of the LED pin
+const int ledPin        = 13; 
 const int ledPin1      = 12;
 const int ledPin2      = 11;
 
-// variables will change:
-int  initial    = 0;       //hold current  initial
-int oldstate    = 0;       //hold last  initial
-int buttonstate = 0;      // variable for reading the pushbutton status
+int  initial    = 0;       
+int oldstate    = 0;       
+int buttonstate = 0;       
 int dancestate = 0;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);   // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);   
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
-  pinMode(buttonPin, INPUT); // initialize the pushbutton pin as an input:
-  pinMode(danceButton, INPUT); // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);  
+  pinMode(danceButton, INPUT);  
 }
-void loop(){
-  //debouncing routline to read button
-  buttonstate = digitalRead(buttonPin);  //state the  initial of button
-  dancestate = digitalRead(danceButton);  //state the  initial of button
-  if(buttonstate == HIGH){               //check if it has been pressed 
+void loop(){ 
+  buttonstate = digitalRead(buttonPin);  
+  dancestate = digitalRead(danceButton);   
+  if(buttonstate == HIGH){             
     delay(50);
-    buttonstate = digitalRead(buttonPin);//state button again
-    if(buttonstate == LOW){              //if it is 0 considered one press
-     initial = oldstate + 1;        //increase  initial by 1
+    buttonstate = digitalRead(buttonPin); 
+    if(buttonstate == LOW){       
+     initial = oldstate + 1;         
     }
-  }else{                          //check if it has been NOT pressed
+  }else{                   
       delay(100);
       }
-   switch (initial){               //react to button press a  initial
-     case 1:                     //if  initial is 1
+   switch (initial){            
+     case 1:                   
        digitalWrite(ledPin, HIGH);//on
        digitalWrite(ledPin1, LOW);//off
        digitalWrite(ledPin2, LOW);//off
-       oldstate =  initial;               //set oldstate  initial as current  initial
+       oldstate =  initial;              
        break;
      case 2:
        digitalWrite(ledPin, LOW);
@@ -51,7 +47,7 @@ void loop(){
        digitalWrite(ledPin2, HIGH);
        oldstate =  initial;
        break;         
-     default:                      //if  initial is not 1 2 3
+     default:                 
        digitalWrite(ledPin, LOW);  //off
        digitalWrite(ledPin1, LOW);
        digitalWrite(ledPin2, LOW);
@@ -74,7 +70,7 @@ void loop(){
         digitalWrite(ledPin1, LOW);
         digitalWrite(ledPin2, LOW);
         delay(400);
-        digitalWrite(ledPin, HIGH);  //off
+        digitalWrite(ledPin, HIGH); 
         digitalWrite(ledPin1, HIGH);
         digitalWrite(ledPin2, HIGH);
         delay(1000);
